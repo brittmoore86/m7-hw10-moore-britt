8801bb3cfbda639030ca39f7bb930907ce7e4121
brittmoore86 <brittmoore86@gmail.com>
2021-07-11 14:14:41 -0400

    JS

--- unsolved/index.js
+++ unsolved/index.js
@@ -4,9 +4,29 @@ var formEl = document.querySelector('form')
 var clear = document.querySelector('#clear')
 var textarea = document.querySelector('textarea')
 
+
+
 // Retrieve name and note content from cookies and localstorage
 // Then apply them to elements on the page
+ // YOUR CODE HERE
 
+//  localstorage https://www.youtube.com/watch?v=AUOzvFzdIk4
+let username = {
+  name: ""
+};
+
+let username_serialized = JSON.stringify(username);
+localStorage.setItem("username", username_serialized);
+console.log(localStorage);
+
+//  cookies
+ document.cookie = "username=; path=/";
+
+ var count = parseInt(document.cookie) || 0
+  formEl.onclick = function() {
+  count ++
+  document.cookie = count
+ }
 
 
 formEl.onsubmit = function(e) {
@@ -14,7 +34,20 @@ formEl.onsubmit = function(e) {
   e.preventDefault()
   // save name element's content to cookies
   // save textarea's content to localstorage
-  var count = parseInt(document.cookie) || 0
+  // YOUR CODE HERE
+
+
+
+
+  function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
 
   // triggers thumbs up animation
   this.elements.save.classList.add('emoji')
